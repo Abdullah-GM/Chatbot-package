f7be44a2d846ced5c450cc7b35cf55cd38bbeff5
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-07-25 15:25:23 +0530

    added react import

--- src/components/chatbot/components/ErrorMessage.jsx
+++ src/components/chatbot/components/ErrorMessage.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import '../Chatbot.css'
 
 const ErrorMessage = ({ errorText = null }) => {
--- src/components/chatbot/components/LoaderMessage.jsx
+++ src/components/chatbot/components/LoaderMessage.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "../Chatbot.css";
 
 function LoaderMessage() {
